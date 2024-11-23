39945a5aa201005d26e88a440b72e49e1d7c59da
Mateus Bazani <mrbazani@gmail.com>
2024-11-23 16:08:57 -0300

    feat: app funcionando e retornando todos os dados da tabela

---
+++ index.js
@@ -0,0 +1,25 @@
+const Database = require('./database/initDb');
+const ApiController = require('./controller');
+const loadCsvToDatabase = require('./database/loadCsv');
+
+
+const PORT = 3000;
+
+async function startServer() {
+    try {
+        const dbInstance = new Database();
+        await dbInstance.initialize();
+
+        const db = dbInstance.getConnection();
+
+        loadCsvToDatabase(db, './movielist.csv');
+        const api = new ApiController(db);
+        api.start(PORT);
+
+    } catch(err) {
+        console.error('Erro ao iniciar a aplicação:', err.message);
+        process.exit(1);
+    }
+}
+
+startServer();
